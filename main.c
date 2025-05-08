d46e53e5905c6f238a4c238894bed5f11af15c30
Rajesh Protap Sarkar <rpsrajesh1212@gmail.com>
2025-05-08 14:43:52 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+SmartCafeManagementSystem/
+├── main.c                
+├── README.md
+├── menu.txt                ← optional: sample menu file
+├── order.txt               ← optional: sample order file
+├── rating.txt              ← optional: sample rating file
+├── stat.txt                ← optional: sample stats file
+├── LICENSE                 ← optional, recommended
